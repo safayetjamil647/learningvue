478cfa7c15feada8a6b5a4daac1e0fc6cec7b543
Safayet Jamil <work.jamil4@gmail.com>
2020-03-11 01:40:00 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NewCompo v-bind:todos="todos"/>
+    {{msg}}
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NewCompo from './components/NewCompo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NewCompo
+  },
+  data(){
+    return{
+      todos:[
+        {
+          id:1,
+          title:"todo one",
+          completed:false
+        },
+        {
+          id:2,
+          title:"todo two",
+          completed:true
+        },
+        {
+          id:3,
+          title:"todo three",
+          completed:true
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/NewCompo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>Todos</h1>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+        <h3>{{todo.title}}</h3>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewCompo',
+    props:["todos"]
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
